96e8531e7b8b1537eda7ffe3e2788d521be3e9c4
rafaelribeiroluz <rafael.ribeiroluz@gmail.com>
2018-07-31 21:25:45 -0300

    Interação com usuário

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -4,9 +4,17 @@ namespace Aula3107_Console
 {
     class Program
     {
+        public static int Id { get; set; } 
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word!");
+
+            Console.WriteLine("Digite um numero: ");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou: " + Id);
+
             Console.ReadKey();
         }
     }
