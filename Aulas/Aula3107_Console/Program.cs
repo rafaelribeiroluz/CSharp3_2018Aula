49139e79a2fc79968b3ad8f79dfbfd454a6e0e8e
rafaelribeiroluz <rafael.ribeiroluz@gmail.com>
2018-08-01 18:27:30 -0300

    Alterar calculadora de if para switch

--- Aulas/Aula3107_Console/Program.cs
+++ Aulas/Aula3107_Console/Program.cs
@@ -4,70 +4,65 @@ namespace Aula3107_Console
 {
     class Program
     {
-        public static int num { get; set; }
-        public static int num2 { get; set; }
-        public static int operador { get; set; }
-        public static double final { get; set; }
+        public static double Num1 { get; set; }
+        public static double Num2 { get; set; }
+        public static string Operador { get; set; }
+        public static double Resultado { get; set; }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite um numero: ");
-            num = int.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o segundo numero: ");
-            num2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Qual o operador?/n");
-            Console.WriteLine("1 - Soma +");
-            Console.WriteLine("2 - Sub -");
-            Console.WriteLine("3 - div /");
-            Console.WriteLine("4 - mult x");
-            operador = int.Parse(Console.ReadLine());
+            Console.Write("1° Numero: ");
+            Num1 = double.Parse(Console.ReadLine());
+            Console.Write("Operador: ");
+            Operador = Convert.ToString(Console.ReadLine());
 
-            if(operador == 1)
-            {
-                final = Soma(num, num2);
-
-            }else if(operador == 2)
-            {
-                final = Sub(num, num2);
-            }else if(operador == 3)
-            {
-                final = Div(num, num2);
-            }else if(operador == 4)
-            {
-                final = Mult(num, num2);
-            }
-
-            Console.WriteLine("Resultado: "+ final);
-            Console.ReadKey();
-        }
-
-        static int Soma(int num, int num2)
-        {
-            int x;
-            x = num + num2;
-            return x;
-
-        }
-
-        static int Sub(int num, int num2)
-        {
-            int x;
-            x = num - num2;
-            return x;
-        }
-
-        static double Div(int num, int num2)
-        {
-            double x;
-            x = num / num2;
-            return x;
-        }
-
-        static int Mult(int num, int num2)
-        {
-            int x;
-            x = num * num2;
-            return x;
+                switch (Operador)
+                {
+                    case "+":
+                        {
+                            Console.Write("2° Numero: ");
+                            Num2 = double.Parse(Console.ReadLine());
+                            Resultado = Num1 + Num2;
+                            Console.WriteLine("Resultado = " + Resultado);
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "-":
+                        {
+                            Console.Write("2° Numero: ");
+                            Num2 = double.Parse(Console.ReadLine());
+                            Resultado = Num1 - Num2;
+                            Console.WriteLine("Resultado = " + Resultado);
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "/":
+                        {
+                            Console.Write("2° Numero: ");
+                            Num2 = double.Parse(Console.ReadLine());
+                            Resultado = Num1 / Num2;
+                            Console.WriteLine("Resultado = " + Resultado);
+                            Console.ReadKey();
+                            break;
+                        }
+                    case "*":
+                        {
+                            Console.Write("2° Numero: ");
+                            Num2 = double.Parse(Console.ReadLine());
+                            Resultado = Num1 * Num2;
+                            Console.WriteLine("Resultado = " + Resultado);
+                            Console.ReadKey();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Digite um operador válido(+|-|*|/)");
+                            Console.ReadKey();
+                            Console.Clear();
+                            Main(null);
+                            break;
+                        }
+                }
         }
     }
 }
