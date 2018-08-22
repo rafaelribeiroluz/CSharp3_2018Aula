02328fa4c6555ef9e90e8790aa70ff47f38070bf
rafaelribeiroluz <rafael.ribeiroluz@gmail.com>
2018-08-21 22:03:02 -0300

    Definindo as propriedades referente as futuras tabelas

---
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace Aula2108_CodeFirst.Modelos.DAL
+{
+
+    public class Contexto : DbContext
+    {
+        public  Contexto() : base("nomeStringConexao")
+        {
+
+        }
+
+        public DbSet<Categoria>  Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+
+    }
+}
