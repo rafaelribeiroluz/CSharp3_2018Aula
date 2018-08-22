5da0b600f5e4bd85b97ed68dae0f1cb0fc18c68e
rafaelribeiroluz <rafael.ribeiroluz@gmail.com>
2018-08-21 21:42:55 -0300

    Criando estrutura da classe produto

---
+++ Aulas/Aula2108_CodeFirst/Modelos/Produto.cs
@@ -0,0 +1,22 @@
+﻿namespace Aula2108_CodeFirst.Modelos
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoID { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public decimal Imposto { get; set; }
+
+        // Relacionamento Produto <-> Categoria
+
+        //Foreigkey
+        public int CategoriaID { get; set; }
+
+        public virtual Categoria _Categoria{ get; set; }
+
+
+
+    }
+}
